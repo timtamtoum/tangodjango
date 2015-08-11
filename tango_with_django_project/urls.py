d35026333f91bf224351c34187c0122760be6bb2
Timothée Brenner <t.brenner@maetvaplanet.com>
2015-08-11 19:04:01 +0200

    Adding a basic view to the rango app (as a test)

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rango import urls as rango_urls
+
 urlpatterns = [
+    url(r'^rango/', include(rango_urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
